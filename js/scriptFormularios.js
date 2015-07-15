e03f2273e6fd489305a6e79e68764538162b27a2
José Fernando Silva <jfernandosilva.her@gmail.com>
2015-07-15 10:04:06 -0500

    Cambio contacto

--- js/scriptFormularios.js
+++ js/scriptFormularios.js
@@ -1,17 +1,24 @@
 $(function() {
 	$("form.subscribe").on("submit", function(e) {
 		e.preventDefault();
-		sendRequest($(this));
+		sendRequestListaCorreo($(this));
 	});
+
+    $("form#formContacto").on("submit", function(e) {
+        e.preventDefault();
+        sendRequestContacto($(this));
+    });
+
 });
 
-function sendRequest(form) {
-	var url = "http://bd-jfsh.rhcloud.com/portal/listaCorreo/saveEmail";
+function sendRequestListaCorreo(form) {
+	//var url = "http://bd-jfsh.rhcloud.com/portal/listaCorreo/saveEmail";
+    var url = "http://localhost:8080/portal/listaCorreo/saveEmail";
     $.ajax({
         type: "POST",
         url: url,
         data: $(form).serialize(),
-        timeout: 5000,
+        timeout: 3000,
         beforeSend: function(jqXHR,settings) {
             $(form).find("#enviarCorreoButton").prop("disabled", true);
             $(form).find("#enviarCorreoButton").val("Enviando...");
@@ -26,13 +33,52 @@ function sendRequest(form) {
         	}
         },
         error: function(XMLHttpRequest,textStatus,errorThrown){
-            //processError("Ocurrio un error al procesar la petición");        
-            console.log("Error");
+            if(textStatus == "timeout") {
+                showToastrError("Error", "No se ha podido establecer conexión con el servidor, intente más tarde.");
+            } else  {
+                showToastrError("Error", "Ocurrió un error al procesar su petición, vuelva a intentar.");
+            }
         },
         complete: function(XMLHttpRequest,textStatus){
             $(form).find("#enviarCorreoButton").val("Enviar");
             $(form).find("#enviarCorreoButton").prop("disabled", false);
             $(form).find("#email").val("");
+        }
+    });
+}
+
+function sendRequestContacto(form) {
+    //var url = "http://bd-jfsh.rhcloud.com/portal/listaCorreo/saveEmail";
+    var url = "http://localhost:8080/portal/contactos/saveContacto";
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: $(form).serialize(),
+        timeout: 3000,
+        beforeSend: function(jqXHR,settings) {
+            $(form).find("#submit_btn").prop("disabled", true);
+            $(form).find("#submit_btn").val("Procesando...");
+        },
+        success: function(response,textStatus){
+            if(response == "true") {
+                showToastrSuccess("Suscripción Exitosa", "Pronto recibirás noticias.");
+            } //else if(response == "Ya Suscrito") {
+            //    showToastrSuccess("Ya estabas suscrito", "En breve seguirás recibiendo noticias.");
+            //} else{
+            //    showToastrError("Ocurrió un error", "Error al intentar guardar su correo, intente nuevamente.");
+            //}
+        },
+        error: function(XMLHttpRequest,textStatus,errorThrown){
+            if(textStatus == "timeout") {
+                showToastrError("Error", "No se ha podido establecer conexión con el servidor, intente más tarde.");
+            } else  {
+                showToastrError("Error", "Ocurrió un error al procesar su petición, vuelva a intentar.");
+            }
+        },
+        complete: function(XMLHttpRequest,textStatus){
+            $(form).find("#submit_btn").prop("disabled", false);
+            $(form).find("#submit_btn").val("Enviar Información");
+            $(form)[0].reset();
         }
     });
 }
@@ -61,21 +107,21 @@ function showToastrSuccess(title, text) {
 
 function showToastrError(title, text) {
 	toastr.options = {
-  		"closeButton": true,
-  		"debug": false,
-  		"newestOnTop": false,
-  		"progressBar": true,
-  		"positionClass": "toast-bottom-left",
-  		"preventDuplicates": true,
-  		"onclick": null,
-  		"showDuration": "1000",
-  		"hideDuration": "1000",
-  		"timeOut": "5000",
-  		"extendedTimeOut": "1000",
-  		"showEasing": "swing",
-  		"hideEasing": "linear",
-  		"showMethod": "fadeIn",
-  		"hideMethod": "fadeOut"
-	}
-	toastr.error(text, title);
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-left",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "1000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    toastr.error(text, title);
 }
