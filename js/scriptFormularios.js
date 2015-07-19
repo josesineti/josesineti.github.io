806164d4d4b2b93f76e05fb8ad5ea83185550381
José Fernando Silva <jfernandosilva.her@gmail.com>
2015-07-18 19:58:10 -0500

    Deploy cv

--- js/scriptFormularios.js
+++ js/scriptFormularios.js
@@ -9,15 +9,57 @@ $(function() {
         sendRequestContacto($(this));
     });
 
+    $("form#formCV").on("submit", function(e) {
+        e.preventDefault();
+        sendRequestCV($(this));
+    });
 });
 
+function sendRequestCV(form) {
+    var url = "http://http://remote.sineti.com:8080/portal/listaCV/saveCV";
+    var data = new FormData($(form)[0]);
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        timeout: 10000,
+        cache: false,
+        contentType: false,
+        processData: false,
+        type: 'POST',     
+        beforeSend: function(jqXHR,settings) {
+            $(form).find("#submit_btn").prop("disabled", true);
+            $(form).find("#submit_btn").val("Enviando...");
+        },
+        success: function(response,textStatus){
+            if(response == "true") {
+                showToastrSuccess("Proceso completado con éxito", "Sus datos fueron guardados correctamente, en breve lo contactaremos");
+            } else{
+                showToastrError("Ocurrió un error", response);
+            }
+        },
+        error: function(XMLHttpRequest,textStatus,errorThrown){
+            if(textStatus == "timeout") {
+                showToastrError("Error", "No se ha podido establecer conexión con el servidor, intente más tarde.");
+            } else  {
+                showToastrError("Error", "Ocurrió un error al procesar su petición, vuelva a intentar.");
+            }
+        },
+        complete: function(XMLHttpRequest,textStatus){
+            $(form).find("#submit_btn").val("Enviar");
+            $(form).find("#submit_btn").prop("disabled", false);
+            $(form)[0].reset();
+        }
+    });
+}
+
 function sendRequestListaCorreo(form) {
-    var url = "http://bd-jfsh.rhcloud.com/portal/listaCorreo/saveEmail";
+    var url = "http://http://remote.sineti.com:8080/portal/listaCorreo/saveEmail";
     $.ajax({
         type: "POST",
         url: url,
         data: $(form).serialize(),
-        timeout: 3000,
+        timeout: 10000,
         beforeSend: function(jqXHR,settings) {
             $(form).find("#enviarCorreoButton").prop("disabled", true);
             $(form).find("#enviarCorreoButton").val("Enviando...");
@@ -47,12 +89,12 @@ function sendRequestListaCorreo(form) {
 }
 
 function sendRequestContacto(form) {
-    var url = "http://bd-jfsh.rhcloud.com/portal/contactos/saveContacto";
+    var url = "http://http://remote.sineti.com:8080/portal/contactos/saveContacto";
     $.ajax({
         type: "POST",
         url: url,
         data: $(form).serialize(),
-        timeout: 3000,
+        timeout: 10000,
         beforeSend: function(jqXHR,settings) {
             $(form).find("#submit_btn").prop("disabled", true);
             $(form).find("#submit_btn").val("Procesando...");
